2eb625c347f2390a8bb3065745d7f7614d3b79c3
ned <ned@ned-devel>
2016-04-22 10:38:57 +0100

    Update

--- src/main/java/com/redmonkeysoftware/sitescraper/app/SiteScraperAppConfiguration.java
+++ src/main/java/com/redmonkeysoftware/sitescraper/app/SiteScraperAppConfiguration.java
@@ -6,11 +6,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import java.beans.PropertyVetoException;
-import java.io.IOException;
-import java.util.Properties;
-import java.util.logging.Level;
 import javax.sql.DataSource;
-import org.apache.velocity.app.VelocityEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,13 +19,10 @@ import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.env.Environment;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.ui.velocity.VelocityEngineFactoryBean;
 
 @Configuration
 @EnableTransactionManagement
@@ -94,36 +87,5 @@ public class SiteScraperAppConfiguration {
         DataSourceTransactionManager dstm = new DataSourceTransactionManager();
         dstm.setDataSource(getDataSource());
         return dstm;
-    }
-
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost(env.getProperty("smtp.host", "smtp.mailgun.org"));
-        mailSender.setPort(env.getProperty("smtp.port", Integer.class, 587));
-        mailSender.setProtocol(env.getProperty("smtp.protocol", "smtp"));
-        mailSender.setUsername(env.getProperty("smtp.username", "postmaster@convey365.com"));
-        mailSender.setPassword(env.getProperty("smtp.password", "9royxs5fb4k8"));
-        Properties props = new Properties();
-        props.setProperty("mail.smtp.starttls.enable", "true");
-        props.setProperty("mail.debug", "true");
-        mailSender.setJavaMailProperties(props);
-        return mailSender;
-    }
-
-    @Bean
-    public VelocityEngine getVelocityEngine() {
-        VelocityEngine engine = null;
-        try {
-            VelocityEngineFactoryBean engineFactoryBean = new VelocityEngineFactoryBean();
-            Properties props = new Properties();
-            props.setProperty("resource.loader", "class");
-            props.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
-            engineFactoryBean.setVelocityProperties(props);
-            engine = engineFactoryBean.createVelocityEngine();
-        } catch (IOException ioe) {
-            java.util.logging.Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, "Error creating Velocity Engine", ioe);
-        }
-        return engine;
     }
 }
